407771b32aba97f9ce0f15d3dc19604c1d1dfe64
amplitud3 <111232829+amplitud3@users.noreply.github.com>
2024-08-02 14:53:31 +0530

    template inheritance

--- app/routes.py
+++ app/routes.py
@@ -1,11 +1,26 @@
-
+from flask import render_template
 from app import app
 
 @app.route('/')
 @app.route('/index')
 
 def index():
-    return "Hello, World!"
+    user = {'username': 'Mark'}
+    posts = [
+        { 
+            'author': {'username': 'Amaan'},
+            'body': 'Beautiful day in portland!'
+        },
 
+        {
+            'author': {'username': 'Nived'},
+            'body': 'The Avengers movie was so cool!'
+        },
 
+        {  
+            'author':{'username': 'Adith'},
+            'body': 'Goa is a beautiful place'
+        }    
+            ]
+    return render_template('index.html', title='Home', user=user,posts=posts)
 
